81fb46ddae5b563516448ec7dd791e37e4394f23
HAITHAM-HK4 <bmwgt32@gmail.com>
2026-05-07 01:35:40 +0300

    Update supabaseClient.ts

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -2,7 +2,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl     = process.env.REACT_APP_SUPABASE_URL     || 'https://umuibrwrjkxlwmbsekih.supabase.co';
-const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVtdWlicndyamt4bHdtYnNla2loIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc5MjMyODAsImV4cCI6MjA5MzQ5OTI4MH0.mJ7oPexYxRSvYhRSEnY2uMga7r1EHZyRAOOcmw-pgEU';
+const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY || 'sb_publishable_NknJTqvqJiQt5BceKJUIow_rHftY-tp';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
